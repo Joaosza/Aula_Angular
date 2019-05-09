385289e409be126228e67891ea00e489db098e42
João Marcos Pereira de Souza <joao.souza@ifpr.paranavai>
2019-05-09 19:11:16 -0300

    Adicionando a Pagina sobre no projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RodapeComponent } from 'src/rodape/rodape.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
   declarations: [
-    AppComponent, HomeComponent, RodapeComponent
+    AppComponent, HomeComponent, RodapeComponent, SobreComponent
   ],
   imports: [
     BrowserModule
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router"; 
 import { HomeComponent } from "./home/home.component";
+import { SobreComponent } from "./sobre/sobre.component";
+import { RodapeComponent } from "src/rodape/rodape.component";
 
 const routes: Routes = [
     {path: '', component: HomeComponent}
+    ,{path: 'sobre', component: SobreComponent}
+    ,{path: 'rodape', component: RodapeComponent} 
 ]
 
 @NgModule({ 
